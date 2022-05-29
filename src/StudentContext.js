5ccf364dec6a2e5c74f2ade8edb2f7bdddd33f2c
sajal4 <sajaldebnath923@gmail.com>
2022-05-29 12:15:59 +0530

    first commit

---
+++ src/StudentContext.js
@@ -0,0 +1,16 @@
+import React, { useContext, useState } from "react";
+
+const StudentContext = React.createContext();
+
+export function StudentContextProvider({children}){
+    const [StudentList, addStudent]=useState([]);
+    return(
+      <StudentContext.Provider value={[StudentList, addStudent]}>
+        {children}
+      </StudentContext.Provider>
+    )
+}
+
+export function useStudentConsumer(){
+    return useContext(StudentContext);
+}
